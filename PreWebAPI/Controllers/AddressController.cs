4516ce85b95ba9ba08d638caeefd586d1992ad5a
thelastdance <54662955+thelastdance@users.noreply.github.com>
2020-07-09 12:05:46 +0300

    prewebapi

---
+++ PreWebAPI/Controllers/AddressController.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Bukimedia.PrestaSharp.Factories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PrestaDeneme.Controllers
+{
+    public class AddressController : Controller
+    {
+        private readonly AddressFactory _adressfactory; // hazır kütüphaneden çekiyor. 
+        string BaseUrl = "http://localhost/prestashop/api";
+        string Account = "V7UIRUC73SBY9ZNAI3GAIN3FKY17E8PV";
+        string Password = "";
+
+        public AddressController()
+        {
+            _adressfactory = new AddressFactory(BaseUrl, Account, Password);
+        }
+
+
+        [HttpGet("/Address")]
+        public IActionResult Index()
+        {
+            return Ok(_adressfactory.GetAll());
+        }
+
+        [HttpGet("/numberofaddress")]
+        public int NumberOfAddresses()
+        {
+            return _adressfactory.GetAll().Count();
+        }
+
+        [HttpGet("/address/{id}")]
+        public IActionResult Address(int id)
+        {
+            var address = _adressfactory.Get(id);
+            return Ok(address);
+        }
+
+
+
+        [HttpPost("/address")]
+        public IActionResult NumberOfAddresses([FromBody]Bukimedia.PrestaSharp.Entities.address address)
+        {
+            _adressfactory.Add(address);
+            return Ok(address);
+        }
+
+        [HttpPut("/address")]
+        public IActionResult UpdateAddress([FromBody] Bukimedia.PrestaSharp.Entities.address address)
+        {
+            var adres = _adressfactory.Get((long)address.id);
+            _adressfactory.Update(adres);
+            return Ok(address);
+        }
+
+        
+
+
+
+
+
+    }
+}
---
+++ PreWebAPI/Controllers/CustomerController.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Bukimedia.PrestaSharp.Factories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PrestaDeneme.Controllers
+{
+    public class CustomerController : Controller
+    {
+        private readonly CustomerFactory _customerfactory; // hazır kütüphaneden çekiyor. 
+        string BaseUrl = "http://localhost/prestashop/api";
+        string Account = "V7UIRUC73SBY9ZNAI3GAIN3FKY17E8PV";
+        string Password = "";
+
+        public CustomerController()
+        {
+            _customerfactory = new CustomerFactory(BaseUrl, Account, Password); //kütüphanenin inputları
+        }
+        [HttpGet("/customer")]
+        public IActionResult Index()
+        {
+            return Ok(_customerfactory.GetAll());
+        }
+
+        [HttpGet("/customer/{id}")]
+        public IActionResult Customer(int id)
+        {
+            var customer = _customerfactory.Get(id);
+
+            return Ok(customer);
+        }
+
+        [HttpGet("/numberofcustomers")]
+        public int NumberOfCustomers()
+        {
+            
+            
+             
+            return _customerfactory.GetAll().Count();
+            
+            
+        }
+
+        // yeni customer ekleme
+        [HttpPost("/addcustomer")]
+        public IActionResult AddCustomer([FromBody]Bukimedia.PrestaSharp.Entities.customer customer) //hazır model
+        {
+            _customerfactory.Add(customer);
+            return Ok(customer);
+        } 
+
+        [HttpPut("/updatecustomer")]
+        public IActionResult UpdateCustomer([FromBody] Bukimedia.PrestaSharp.Entities.customer customer)
+        {
+            var musteri = _customerfactory.Get((long)customer.id);
+            _customerfactory.Update(customer);
+            return Ok(customer);
+        }
+
+
+        [HttpDelete("/deletecustomer/{id}")]
+        public IActionResult DeleteCustomer(long id)
+        {
+            var musteri = _customerfactory.Get(id);
+            _customerfactory.Delete(musteri);
+            return Ok();
+        }
+    }
+}
