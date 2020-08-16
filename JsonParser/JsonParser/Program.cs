21da01a110d4e630bd8654417c097885380c6379
kcemacar <kutaycemacar2@gmail.com>
2020-08-16 19:54:50 +0300

    jsonparser

---
+++ JsonParser/JsonParser/Program.cs
@@ -0,0 +1,35 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace JsonParser
+{
+    class Program // First download Newtonsoft.Json
+    {
+        static void Main(string[] args)
+        {
+            Student student = new Student()
+            {
+                Id = 1,
+                Name = "Kutay",
+                Degree = "MCA",
+                Hobbies = new List<string>()
+                {
+                    "Reading",
+                    "Playing Games"
+                }
+            };
+
+            string strResultJson = JsonConvert.SerializeObject(student);
+            File.WriteAllText(@"student.json", strResultJson);
+            Console.WriteLine("Stored ! ");
+
+            strResultJson = String.Empty;
+            strResultJson = File.ReadAllText(@"student.json");
+            Student resultstudent = JsonConvert.DeserializeObject<Student>(strResultJson);
+            Console.WriteLine(resultstudent.ToString());
+            
+        }
+    }
+}
---
+++ JsonParser/JsonParser/Student.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JsonParser
+{
+    class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Degree { get; set; }
+        public List<string> Hobbies { get; set; }
+
+        public override string ToString()
+        {
+            return string.Format("Student Information:" +
+                "\n\n\tId: {0}, " +
+                "\n\tName: {1}, " +
+                "\n\tDegree: {2}, " +
+                "\n\tHobbies : {3}", 
+                Id, 
+                Name, 
+                Degree, 
+                string.Join(",",Hobbies.ToArray())
+                );
+        }
+    }
+}
